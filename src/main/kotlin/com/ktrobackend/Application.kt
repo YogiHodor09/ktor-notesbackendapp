6ef6d5bfc164d0021f43b177da3827785dca21d6
TECHMAHINDRA\YP00818780 <yp00818780@techmahindra.com>
2022-04-19 10:51:57 +0530

    CRUD Operations added !!

--- src/main/kotlin/com/ktrobackend/Application.kt
+++ src/main/kotlin/com/ktrobackend/Application.kt
@@ -1,5 +1,6 @@
 package com.ktrobackend
 
+import com.ktrobackend.entities.NoteEntity
 import com.ktrobackend.plugins.aboutUsModule
 import com.ktrobackend.plugins.configureRouting
 import io.ktor.serialization.kotlinx.json.*
@@ -8,6 +9,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import org.ktorm.database.Database
+import org.ktorm.dsl.*
 
 
 fun main() {
@@ -26,6 +28,34 @@ fun main() {
             user = "root",
             password = "admin"
         )
+        
+        /* CRUD OPERATIONS IN DB */
+        // insert values to DB
+        /*database.insert(NoteEntity) {
+            set(it.note, "Study Ktor")
+        }*/
+
+        // update data in DB
+
+        database.update(NoteEntity) {
+            set(it.note, "Become a FullStack Developer")
+            where {
+                it.id eq 4
+            }
+        }
+
+        // delete data in DB
+
+        database.delete(NoteEntity) {
+            it.id eq 5
+        }
+
+        // read the values from DB
+        val notesData = database.from(NoteEntity).select()
+        for (rowData in notesData) {
+            println("${rowData[NoteEntity.id]} : ${rowData[NoteEntity.note]}")
+        }
+
     }.start(wait = true)
 }
 
